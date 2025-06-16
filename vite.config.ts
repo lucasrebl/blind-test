e42a03d7aa08d7329dea09add56f905a25d8eb43
lucasrebl <rebouletlucas@gmail.com>
2025-06-16 15:25:34 +0200

    fix error config ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
+// In your vite.config.ts file
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath } from 'url'
 import path from 'path'
 
-// https://vitejs.dev/config/
+// Create __dirname equivalent in ESM
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
-  }
+  },
+  // Autres configurations selon vos besoins
 })
